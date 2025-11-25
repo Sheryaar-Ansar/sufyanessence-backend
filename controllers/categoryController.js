5cb5f5a50e8cad900178719ad6f4be75aad4ae4d
Sheryaar <Sheryaarlong@gmail.com>
2025-11-25 18:59:10 +0500

    fix category delete

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -62,7 +62,7 @@ const  Category  = require('../models/Category');
         if(!category) {
             return res.status(404).json({ message: 'Category not found' });
         }
-        await category.remove();
+        await category.deleteOne();
         res.status(200).json({ message: 'Category deleted successfully' });
     } catch (error) {
         res.status(500).json({ message: 'Server error', error: error.message });
